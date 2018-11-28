91d1b1f4123a32686b76160180e79e188bf48c76
YaoZhun <mopackp47557@gmail.com>
2018-11-28 10:45:40 +0800

    20181110

--- php/photoEditApi/user_photo_read.php
+++ php/photoEditApi/user_photo_read.php
@@ -1,5 +1,5 @@
 <?php
-if(! isset($_SESSION['seller'])){
+if(! isset($_SESSION['user']['user_photo'])){
     echo json_encode($result, JSON_UNESCAPED_UNICODE);
     exit;
 }
@@ -7,7 +7,7 @@ $result = [
     'success' => true,
     'resultCode' => 200,
     'errorMsg' => '',
-    'data' => $_SESSION['seller']
+    'data' => $_SESSION['user']['user_photo']
 ];
 
 echo json_encode($result, JSON_UNESCAPED_UNICODE);
