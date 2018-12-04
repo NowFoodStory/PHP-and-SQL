b691f690268e42322b93bcc4318d26f5b1bdc3a2
YaoZhun <mopackp47557@gmail.com>
2018-12-04 18:38:28 +0800

    完成合併的陣列

--- php/shoppnAPI/shoppnAPI.php
+++ php/shoppnAPI/shoppnAPI.php
@@ -10,14 +10,10 @@ foreach( $seller as $sellerID){
     $seller_sid = $sellerID['seller_sid'];
     $seller_name = $sellerID['seller_name'];
     $sql1 = " SELECT * FROM `food_commodity` WHERE `seller_sid`=$seller_sid";
-    json_encode($Data, JSON_UNESCAPED_UNICODE);
-    // var_export($Data);
-    $Datas = array_push($Data,$seller_sid,$seller_sid);
-    // var_export($Datas);
-    $suckmydick=array_push($Data,$seller_sid,$seller_sid);
-    var_export($Datas);
-    // echo json_encode($Data1, JSON_UNESCAPED_UNICODE);
-    // echo json_encode($Datas, JSON_UNESCAPED_UNICODE);
+    $stmt1 = $pdo->prepare($sql1);
+    $stmt1->execute();
+    $seller1 = $stmt1->fetchAll(PDO::FETCH_ASSOC);
+    print_r(array_merge($sellerID,$seller1));
 };
 
 
