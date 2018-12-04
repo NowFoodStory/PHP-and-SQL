a94138427baffe0b618f11037f9b9025079a6c91
YaoZhun <mopackp47557@gmail.com>
2018-12-04 23:27:36 +0800

    201812041127

--- php/shoppnAPI/shoppnAPI.php
+++ php/shoppnAPI/shoppnAPI.php
@@ -1,7 +1,7 @@
 <?php
 require __DIR__.'/../__connect_db.php';
 
-$sql =  "SELECT seller_sid , seller_name FROM seller_initial ";
+$sql =  "SELECT seller_sid , seller_name , logo_photo FROM seller_initial ";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
 $seller = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -9,12 +9,16 @@ json_encode($seller, JSON_UNESCAPED_UNICODE);
 foreach( $seller as $sellerID){
     $seller_sid = $sellerID['seller_sid'];
     $seller_name = $sellerID['seller_name'];
+    $seller_photo = $sellerID['logo_photo'];
     $sql1 = " SELECT * FROM `food_commodity` WHERE `seller_sid`=$seller_sid";
     $stmt1 = $pdo->prepare($sql1);
     $stmt1->execute();
     $seller1 = $stmt1->fetchAll(PDO::FETCH_ASSOC);
-    print_r(array_merge($sellerID,$seller1));
+    $sp = (array_merge($sellerID,$seller1));
+    echo json_encode($sp, JSON_UNESCAPED_UNICODE);
+
 };
+
 
 
 // print_r($seller_name);
