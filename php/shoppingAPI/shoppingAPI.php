e1b7c1229a1da7118b9f3f99580b8134719005fe
YaoZhun <mopackp47557@gmail.com>
2018-12-05 14:38:58 +0800

    123456

--- php/shoppingAPI/shoppingAPI.php
+++ php/shoppingAPI/shoppingAPI.php
@@ -1,22 +1,32 @@
 <?php
 require __DIR__.'/../__connect_db.php';
 
-$sql =  "SELECT seller_sid , seller_name , logo_photo FROM seller_initial ";
+// $sql =  "SELECT seller_sid , seller_name FROM seller_initial ";
+$sql =  "SELECT s.seller_sid , s.seller_name, f.food_name
+        FROM seller_initial s 
+        JOIN food_commodity f ON s.seller_sid=f.seller_sid
+        ORDER BY s.seller_sid, f.food_sid ";
+
+
+
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
 $seller = $stmt->fetchAll(PDO::FETCH_ASSOC);
-json_encode($seller, JSON_UNESCAPED_UNICODE);
+echo json_encode($seller, JSON_UNESCAPED_UNICODE);
+exit;
 foreach( $seller as $sellerID){
     $seller_sid = $sellerID['seller_sid'];
     $seller_name = $sellerID['seller_name'];
-    $seller_photo = $sellerID['logo_photo'];
-    $sql1 = " SELECT * FROM `food_commodity` WHERE `seller_sid`=$seller_sid";
+    $sql1 = " SELECT food_name FROM `food_commodity` WHERE `seller_sid`=$seller_sid";
     $stmt1 = $pdo->prepare($sql1);
     $stmt1->execute();
     $seller1 = $stmt1->fetchAll(PDO::FETCH_ASSOC);
-    $sp = (array_merge($seller,$seller1));
-    echo json_encode($sp, JSON_UNESCAPED_UNICODE);
+    $seller_sp = array($sellerID);
+    $sp = (array($seller_sp,$seller1));
+    print_r($sp);
+    // echo json_encode($sp, JSON_UNESCAPED_UNICODE);
 
+    
 };
 
 
---
+++ php/shoppingAPI/shoppingAPI_02.php
@@ -0,0 +1,27 @@
+<?php
+require __DIR__.'/../__connect_db.php';
+
+$s_sql =  "SELECT seller_sid , seller_name FROM seller_initial ";
+$s_stmt = $pdo->query($s_sql);
+$seller = $s_stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$f_sql =  "SELECT * FROM `food_commodity` WHERE 1";
+$f_stmt = $pdo->query($f_sql);
+$fc = $f_stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$food = [];
+foreach($fc as $f){
+    $food[$f['seller_sid']][] = $f;
+}
+
+
+
+
+
+
+$result = [
+    'seller' => $seller,
+    'food' => $food,
+];
+
+echo json_encode($result, JSON_UNESCAPED_UNICODE);
