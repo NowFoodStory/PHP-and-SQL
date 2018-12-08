23b5b669506556816337e354975f5d58657622da
YaoZhun <mopackp47557@gmail.com>
2018-12-09 00:35:24 +0800

    訂單系統
    只做到判斷會員是否有登入

---
+++ php/orders/ordersAPI.php
@@ -0,0 +1,38 @@
+<?php
+require __DIR__.'/../__connect_db.php';
+
+
+header('Content-Type: application/json');
+$from_orders = true;
+
+$method = $_SERVER['REQUEST_METHOD'];
+$body = file_get_contents('php://input');
+$body = json_decode($body, true);
+
+$result = [
+    'success' => false,
+    'resultCode' => 400,
+    'errorMsg' => '用戶沒有登入',
+];
+
+if(! isset($_SESSION['seller'])){
+    echo json_encode($result,JSON_UNESCAPED_UNICODE);
+}elseif(! isset($_SESSION['user'])){
+    echo json_encode($result,JSON_UNESCAPED_UNICODE);
+}else{
+    echo json_encode($result,JSON_UNESCAPED_UNICODE);
+}
+
+//POST
+switch ($method){
+    case 'GET':
+    require __DIR__.'/ordersGET.php';
+    exit;
+    case 'POST':
+    require __DIR__.'/ordersPUT.php';
+    exit;
+    default:
+    $result['resultCode'] = 401;
+    $result['errorMsg'] = '錯誤的 HTTP method';
+}
+echo json_encode($result, JSON_UNESCAPED_UNICODE);
