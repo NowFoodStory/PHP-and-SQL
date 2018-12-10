45e1885c822e0afc98c6436bb8638c41ab7cd8d6
YaoZhun <mopackp47557@gmail.com>
2018-12-10 21:03:37 +0800

    20181210

--- php/user_orders/user_orders_API.php
+++ php/user_orders/user_orders_API.php
@@ -14,16 +14,17 @@ if(! isset($_SESSION['user'])){
 }
 $user_id = $_SESSION['user']['user_id'];
 
-$seller_sid="SELECT s.seller_name,s.logo_photo,s.seller_address,s.opening,s.close_time,s.seller_phone 
-FROM order_deta AS d JOIN seller_initial AS s ON d.seller_sid = s.seller_sid
+$seller_sid="SELECT s.seller_sid,s.seller_name,d.Numb_sid 
+FROM order_deta AS d 
+JOIN seller_initial AS s 
+ON d.seller_sid = s.seller_sid 
 WHERE d.user_id = ?";
 
 $stmt = $pdo->prepare($seller_sid);
 $stmt->execute([$user_id]);
 $seller = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$foodsql="SELECT o.food_name,o.food_photo,o.food_quantity,o.food_discount
-FROM order_deta AS d 
+$foodsql="SELECT o.food_name,o.food_photo,o.food_quantity,o.food_discount FROM order_deta AS d 
 JOIN orders AS o 
 ON d.Numb_sid = o.Numb_sid
 WHERE d.Numb_sid = ?";
