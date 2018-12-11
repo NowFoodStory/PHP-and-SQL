0702448f0396fa850c105048a8c4bf4563065141
YaoZhun <mopackp47557@gmail.com>
2018-12-11 19:52:21 +0800

    修改

--- php/seller_orders/seller_read_orders_API.php
+++ php/seller_orders/seller_read_orders_API.php
@@ -7,9 +7,7 @@ $result = [
     'resultCode' => 400,
     'errorMsg' => '用戶沒有登入',
 ];
-
-
-if (!isset($_SESSION['user'])) {
+if (!isset($_SESSION['seller'])) {
     echo json_encode($result, JSON_UNESCAPED_UNICODE);
     exit;
 }
@@ -17,11 +15,11 @@ $entityBody = file_get_contents('php://input');
 $bdata = json_decode($entityBody, true);
 $result['food'] = $bdata;
 
-$sql = "SELECT food_sid,food_name,food_photo,food_quantity,food_discount FROM orders WHERE Numb_sid=?";
+$sql = "SELECT food_sid,food_name,food_photo,food_quantity,food_discount FROM orders 
+ WHERE Numb_sid=?";
 $stmt = $pdo->prepare($sql);
 $stmt->execute([
     $bdata['Numb_sid']
 ]);
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 echo json_encode($result, JSON_UNESCAPED_UNICODE);
