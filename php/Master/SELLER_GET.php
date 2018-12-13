e2da9420a651e4cfb884bb20a1cc80b09d57ec45
YaoZhun <mopackp47557@gmail.com>
2018-12-13 11:35:38 +0800

    修改

---
+++ php/Master/SELLER_GET.php
@@ -0,0 +1,10 @@
+<?php
+
+$sql="SELECT `seller_sid`,`seller_name`,`seller_phone`,`seller_email`,`seller_status`
+ FROM `seller_initial` WHERE 1 and `seller_status`=0";
+$stmt = $pdo->query($sql);
+$seller_data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+echo json_encode($seller_data,JSON_UNESCAPED_UNICODE);
---
+++ php/Master/SELLER_PUT.php
@@ -0,0 +1,32 @@
+<?php
+$result = [
+    'success' => false,
+    'resultCode' => 400,
+    'errorMsg' => '用戶沒有登入',
+];
+
+
+if (!isset($_SESSION['user'])) {
+    echo json_encode($result, JSON_UNESCAPED_UNICODE);
+    exit;
+}
+$entityBody = file_get_contents('php://input');
+$bdata = json_decode($entityBody, true);
+$result['food'] = $bdata;
+
+$sql = "UPDATE `seller_initial` SET `seller_status`=1 WHERE `seller_sid` =?";
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute([
+    $bdata['user_id']
+]);
+
+if($stmt->rowCount()==1){
+    $result['success'] = true;
+    $result['resultCode'] = 200;
+    $result['errorMsg'] = '修改成功';
+} else {
+    $result['resultCode'] = 408;
+    $result['errorMsg'] = '修改失敗';
+}
+echo json_encode($result, JSON_UNESCAPED_UNICODE);
