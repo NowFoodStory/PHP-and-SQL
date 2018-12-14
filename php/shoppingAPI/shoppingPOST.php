3946a9668ab89c47319de4476a07d3775af44d15
YaoZhun <mopackp47557@gmail.com>
2018-12-15 02:15:47 +0800

    Update shoppingPOST.php

--- php/shoppingAPI/shoppingPOST.php
+++ php/shoppingAPI/shoppingPOST.php
@@ -31,14 +31,20 @@ $fc = $f_stmt->fetchAll(PDO::FETCH_ASSOC);
 if (empty($bdata['place']) && empty($bdata['search'])) {
     $s_sql = "SELECT seller_sid,`seller_name`,`opening`,`close_time`,`logo_photo`,`lng`,`lat` FROM seller_initial ";
 }else if(!empty($bdata['place']) && empty($bdata['search'])) {
+    // echo '條件:地址欄位帶值';
     $a = '\''.'%'.$bdata['place'].'%'.'\'';
+    $pdo->quote($a);
     $s_sql = "SELECT seller_sid,`seller_name`,`opening`,`close_time`,`logo_photo`,`lng`,`lat` FROM seller_initial WHERE seller_address LIKE $a";
 }else if(empty($bdata['place']) && !empty($bdata['search'])){
-    $b = '\''.'%'.$bdata['search'] .'%'.'\'';
-    $s_sql = "SELECT seller_sid,`seller_name`,`opening`,`close_time`,`logo_photo`,`lng`,`lat` FROM seller_initial WHERE seller_address LIKE $b";
+    // echo '條件:搜尋框帶值';
+    // $pdo->quote($b);
+    $b = '\''.'%'.$bdata['search'].'%'.'\'';
+    $s_sql = "SELECT seller_sid,`seller_name`,`opening`,`close_time`,`logo_photo`,`lng`,`lat` FROM seller_initial WHERE seller_name LIKE $b";
 }else{
+    // echo '條件:兩個都有值';
     $a = '\''.'%'.$bdata['place'].'%'.'\'';
     $b = '\''.'%'.$bdata['search'].'%'.'\'';
+    // $pdo->quote($a,$b);
     $s_sql = "SELECT seller_sid,`seller_name`,`opening`,`close_time`,`logo_photo`,`lng`,`lat` FROM seller_initial WHERE seller_address LIKE $a and seller_name LIKE $b";
 }
 
